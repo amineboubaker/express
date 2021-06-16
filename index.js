c7b55d6fc1064eddb9cc456252c4ab07be9aa080
amineboubaker <amine.boubaker001@gmail.com>
2021-06-16 16:28:29 +0200

    express updated

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ const path = require("path");
 
 app.use(
   (addActiveTime = (req, res, next) => {
-    let requestAt = new Date().getHours();
+    let requestAt = new Date().getDay();
 
-    if (requestAt <= 9 || requestAt >= 22) {
-      res.send("no access");
+    if (requestAt <= 1 || requestAt >= 5) {
+      res.send("You can visit us From Monday to Friday");
     } else {
       next();
     }
   })
 );
+app.use(
+  (addActiveTime = (req, res, next) => {
+    let requestAt = new Date().getHours();
+
+    if (requestAt <= 9 || requestAt >= 17) {
+      res.send("You can visit us from 9 to 17");
+    } else {
+      next();
+    }
+  })
+);
+
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/Home.html"));
 });
-// app.get('/ContactUs',(req,res)=>{
-//     res.sendFile(path.join(__dirname + '/public/ContactUs.html'))
-// })
-// app.get('/ourService',(req,res)=>{
-//     res.sendFile(path.join(__dirname + '/public/OurServices.html'))
-// })
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) =>
   err ? console.error(err) : console.log(`server is running on PORT ${PORT}`)
